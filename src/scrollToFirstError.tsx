3cba31f463633fc490939c732148cdab8fe4b7c7
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-04-10 11:54:42 +0100

    WIP

--- src/scrollToFirstError.tsx
+++ src/scrollToFirstError.tsx
@@ -1,9 +1,9 @@
 export function scrollToFirstError() {
-  // Try finding the first error message.
+  // Try finding the first error message
   const firstErrorMessage =
     document.querySelector<HTMLElement>('[data-field-error]')
 
-  // If we can't find it, log a warning and don't do anything else.
+  // If we can't find it, log a warning and don't do anything else
   if (!firstErrorMessage) {
     console.warn('Failed to find error field')
     return
@@ -12,13 +12,13 @@ export function scrollToFirstError() {
   const inputId = firstErrorMessage.dataset.fieldError
 
   // We found the error message. Let's try to find the corresponding field label
-  // and scroll to it if available.
+  // and scroll to it
   const label = document.querySelector(`[for="${inputId}"]`)
   if (label) {
     label.scrollIntoView()
     return
   }
 
-  // Failed to find the label. Let's scroll to the error message insteage
+  // Failed to find the label. Let's scroll to the error message instead
   firstErrorMessage.scrollIntoView()
 }
